c9faa8fe052eb954c69e9c3b64ab9f51e6135d6f
cohedx7 <kimkh87@gmail.com>
2022-03-23 18:26:40 +0100

    started over from scratch

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const container = document.getElementById('container');
+/*const container = document.getElementById('container');
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        cell.innerText = (c);
+        //cell.innerText = (c);
         container.appendChild(cell).className = "grid-item";
     }
 }
 
-makeRows(16, 16);
+makeRows(16, 16);*/
+
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'gray';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        populateBoard(input);
+    } else {
+        console.log("Too many squares.");
+    }
+}
